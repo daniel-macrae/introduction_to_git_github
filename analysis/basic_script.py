29443ff02c341694957a2f5ffcc88e772733913c
Daniel MacRae <61056860+daniel-macrae@users.noreply.github.com>
2025-11-04 11:09:49 +0100

    test script

--- analysis/basic_script.py
+++ analysis/basic_script.py
@@ -4,3 +4,8 @@ def load_data():
     file_path = 'data/sample_data.csv'
     df = pd.read_csv(file_path)
     return df
+
+
+if __name__ == "__main__":
+    data = load_data()
+    print(data.head())
